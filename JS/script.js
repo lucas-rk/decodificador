3b1635716cc7f781d459dba477ecf246d66e2343
lucas-rk <lucas.rioliveira150@gmail.com>
2025-06-23 00:43:45 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,36 +1,45 @@
 var textInput = document.querySelector("#input-texto");
 var outInput = document.querySelector("#output");
 
-
-function btnCriptografar(){
-
+function btnCriptografar() {
   var texto = textInput.value;
 
-  var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
+  var resultCripto = texto
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
 
-
-  document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultCripto + 
-  '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+  document.getElementById("output").innerHTML =
+    '<textarea readonly id="resultado">' +
+    resultCripto +
+    "</textarea>" +
+    '<button class="btn-copiar" onclick="copiar()">Copiar</button>';
 }
 
-function btnDescriptografar(){
+function btnDescriptografar() {
+  var texto = textInput.value;
 
-    var texto = textInput.value;
+  var resultDescripto = texto
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
 
-    var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
-
-    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultDescripto + 
-    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+  document.getElementById("output").innerHTML =
+    '<textarea readonly id="resultado">' +
+    resultDescripto +
+    "</textarea>" +
+    '<button class="btn-copiar" onclick="copiar()">Copiar</button>';
 }
 
 function copiar() {
-  const textoCop = document.querySelector('#output h2').innerText;
+  const textoCop = document.getElementById("resultado");
 
-  navigator.clipboard.writeText(textoCop)
-    .then(() => {
-      alert("Texto copiado.");
-    })
-    .catch(() => {
-      alert("Erro ao copiar o texto.");
-    });
+  textoCop.select();
+  document.execCommand("copy");
+
+  alert("Texto copiado.");
 }
