02a2270bdb2557e60eb562ac1c599ea4e785a7bc
lucas-rk <lucas.rioliveira150@gmail.com>
2025-06-23 01:01:33 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -9,11 +9,8 @@ function btnCriptografar(){
   var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
 
 
-  document.getElementById('output').innerHTML = 
-  '<textarea readonly id="resultado">' + resultCripto + '</textarea>' + 
-  '<button class="btn-copiar" onclick="copiar()">Copiar</button>';
-
-
+  document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultCripto + 
+  '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
 function btnDescriptografar(){
@@ -23,7 +20,8 @@ function btnDescriptografar(){
     var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
 
     document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultDescripto + 
-    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>';
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+}
 }
 
 function copiar() {
