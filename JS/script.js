c7771ffdf25812e0931a98a7209144da04c33d42
lucas-rk <lucas.rioliveira150@gmail.com>
2025-06-23 00:44:52 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,26 @@
 var textInput = document.querySelector("#input-texto");
 var outInput = document.querySelector("#output");
 
-function btnCriptografar() {
+
+function btnCriptografar(){
+
   var texto = textInput.value;
 
-  var resultCripto = texto
-    .replace(/e/g, "enter")
-    .replace(/i/g, "imes")
-    .replace(/a/g, "ai")
-    .replace(/o/g, "ober")
-    .replace(/u/g, "ufat");
+  var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
 
-  document.getElementById("output").innerHTML =
-    '<textarea readonly id="resultado">' +
-    resultCripto +
-    "</textarea>" +
-    '<button class="btn-copiar" onclick="copiar()">Copiar</button>';
+
+  document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultCripto + 
+  '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
-function btnDescriptografar() {
-  var texto = textInput.value;
+function btnDescriptografar(){
 
-  var resultDescripto = texto
-    .replace(/enter/g, "e")
-    .replace(/imes/g, "i")
-    .replace(/ai/g, "a")
-    .replace(/ober/g, "o")
-    .replace(/ufat/g, "u");
+    var texto = textInput.value;
 
-  document.getElementById("output").innerHTML =
-    '<textarea readonly id="resultado">' +
-    resultDescripto +
-    "</textarea>" +
-    '<button class="btn-copiar" onclick="copiar()">Copiar</button>';
+    var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+
+    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultDescripto + 
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
 function copiar() {
